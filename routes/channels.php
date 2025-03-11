1e5291d4c3c459fb28cb57647d36e5a569b09ca8
Maverick Chan <maverick_4@msn.com>
2025-03-11 10:31:11 +0800

    fixed the catch-all route

--- routes/channels.php
+++ routes/channels.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
+
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::any('/{any}', function () {
     return view('welcome');
-})->where(['any' => '.*']);
+})->where(['any' => '^(?!api\/).*']);
+
+// Route::any('/', function () {
+//     return view('welcome');
+// });
